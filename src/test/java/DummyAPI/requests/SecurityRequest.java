cca2d493d389a2d030dcd2693f4315a876e2cfef
Resnu Wilmar <resnuwilmar1@gmail.com>
2026-09-17 13:35:10 +0700

    add security for general testing endpoint

---
+++ src/test/java/DummyAPI/requests/SecurityRequest.java
@@ -0,0 +1,23 @@
+package DummyAPI.requests;
+
+import DummyAPI.configs.ApiConfig;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class SecurityRequest {
+
+    public static Response getWithoutAppId(String endpoint) {
+        return RestAssured.given()
+                .baseUri(ApiConfig.BASE_URL)
+                .when()
+                .get(endpoint);
+    }
+    
+    public static Response getWithCustomAppId(String endpoint, String customAppId) {
+        return RestAssured.given()
+                .baseUri(ApiConfig.BASE_URL)
+                .header("app-id", customAppId)
+                .when()
+                .get(endpoint);
+    }
+}
--- src/test/java/DummyAPI/stepdef/SecurityStepDef.java
+++ src/test/java/DummyAPI/stepdef/SecurityStepDef.java
@@ -1,9 +1,8 @@
 package DummyAPI.stepdef;
 
-import DummyAPI.configs.ApiConfig;
 import DummyAPI.context.TestContext;
+import DummyAPI.requests.SecurityRequest;
 import io.cucumber.java.en.When;
-import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
 public class SecurityStepDef {
@@ -16,22 +15,13 @@ public class SecurityStepDef {
 
     @When("I send request to {string} without app-id")
     public void iSendRequestToEndpointWithoutAppId(String endpoint) {
-        Response response = RestAssured.given()
-                .baseUri(ApiConfig.BASE_URL)
-                .when()
-                .get(endpoint);
-
+        Response response = SecurityRequest.getWithoutAppId(endpoint);
         context.setResponse(response);
     }
 
     @When("I send request to {string} with invalid app-id {string}")
     public void iSendRequestToEndpointWithInvalidAppId(String endpoint, String invalidAppId) {
-        Response response = RestAssured.given()
-                .baseUri(ApiConfig.BASE_URL)
-                .header("app-id", invalidAppId)
-                .when()
-                .get(endpoint);
-
+        Response response = SecurityRequest.getWithCustomAppId(endpoint, invalidAppId);
         context.setResponse(response);
     }
 }
